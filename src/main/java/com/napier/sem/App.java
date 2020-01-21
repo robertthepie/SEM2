614ba22ccba6580809a52f7cc65c710f59746417
robertthepie <oliwierworoniecki@gmail.com>
2020-01-21 17:48:21 +0000

    Fixed Compiler Error

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,7 @@
 package com.napier.sem;
 
 public class App {
-    public static void Main(String[]args)
+    public static void main(String[]args)
     {
         System.out.println("Test");
     }
